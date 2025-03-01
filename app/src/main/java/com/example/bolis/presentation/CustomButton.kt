5b72f3a9407d3da3facd9d5f943280560836c856
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-03-01 23:53:41 +0500

    Add HugeCustomButton composable with updated styling and layout

--- app/src/main/java/com/example/bolis/presentation/CustomButton.kt
+++ app/src/main/java/com/example/bolis/presentation/CustomButton.kt
@@ -2,14 +2,10 @@ package com.example.bolis.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -40,6 +36,29 @@ fun CustomButton() {
             text = "Log In",
             fontSize = 18.sp,
             fontWeight = FontWeight(600),
+            textAlign = TextAlign.Center,
+            fontFamily = fontFamily,
+            color = Color.White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HugeCustomButton() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(47.dp)
+            .clip(shape = RoundedCornerShape(20.dp))
+            .background(Green50)
+            .clickable(onClick = { }),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Log In",
+            fontSize = 20.sp,
+            fontWeight = FontWeight(900),
             textAlign = TextAlign.Center,
             fontFamily = fontFamily,
             color = Color.White

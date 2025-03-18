7aac306b340e10ce1a3afb7318eed5d62b2f0f5f
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-03-18 23:55:08 +0500

    Update font family to Poppins for improved typography consistency

--- app/src/main/java/com/example/bolis/ui/theme/FontFamily.kt
+++ app/src/main/java/com/example/bolis/ui/theme/FontFamily.kt
@@ -6,12 +6,13 @@ import androidx.compose.ui.text.font.FontWeight
 import com.example.bolis.R
 
 val fontFamily = FontFamily(
-    Font(R.font.sfprodisplay_black, FontWeight.Companion.Black),
-    Font(R.font.sfprodisplay_bold, FontWeight.Companion.Bold),
-    Font(R.font.sfprodisplay_heavy, FontWeight.Companion.ExtraBold),
-    Font(R.font.sfprodisplay_light, FontWeight.Companion.Light),
-    Font(R.font.sfprodisplay_medium, FontWeight.Companion.Medium),
-    Font(R.font.sfprodisplay_regular, FontWeight.Companion.Normal),
-    Font(R.font.sfprodisplay_semibold, FontWeight.Companion.SemiBold),
-    Font(R.font.sfprodisplay_thin, FontWeight.Companion.Thin),
+    Font(R.font.poppins_black, FontWeight.Companion.Black),
+    Font(R.font.poppins_bold, FontWeight.Companion.Bold),
+    Font(R.font.poppins_extrabold, FontWeight.Companion.ExtraBold),
+    Font(R.font.poppins_light, FontWeight.Companion.Light),
+    Font(R.font.poppins_extralight, FontWeight.Companion.ExtraLight),
+    Font(R.font.poppins_medium, FontWeight.Companion.Medium),
+    Font(R.font.poppins_regular, FontWeight.Companion.Normal),
+    Font(R.font.poppins_semibold, FontWeight.Companion.SemiBold),
+    Font(R.font.poppins_thin, FontWeight.Companion.Thin),
 )

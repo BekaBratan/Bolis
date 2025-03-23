7f0ea42799b87f31e530a9d24847d178e4f24d87
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-03-23 16:31:33 +0500

    Add API service setup with Retrofit and OkHttp; define base URL and service builder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,14 @@ dependencies {
 
     // JSON serialization library, works with the Kotlin serialization plugin
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.12.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor")
 }
---
+++ app/src/main/java/com/example/bolis/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.bolis.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "http://www.bolis.com/api/"
+    }
+}

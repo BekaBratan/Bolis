fd30737d99c481f4f8dc53c560def54e2b4b8d1c
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-04-25 00:46:13 +0500

    Add CatalogItem composable; implement UI for displaying catalog items with image, name, location, and status

---
+++ app/src/main/java/com/example/bolis/ui/Elements/CatalogItem.kt
@@ -0,0 +1,125 @@
+package com.example.bolis.ui.Elements
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.bolis.R
+import com.example.bolis.ui.theme.Black50
+import com.example.bolis.ui.theme.Dark50
+import com.example.bolis.ui.theme.Green50
+import com.example.bolis.ui.theme.Grey30
+import com.example.bolis.ui.theme.White40
+import com.example.bolis.ui.theme.White50
+import com.example.bolis.ui.theme.fontFamily
+
+@Preview
+@Composable
+fun CatalogItem(
+    name: String = "Name\nSurn\name",
+    location: String = "Location",
+    status: String = "New",
+    imageUrl: String = "http://bolis.maisa.kz/storage/images/1.jpg",
+    onClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .width(170.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(White50)
+            .padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        AsyncImage(
+            model = imageUrl, // Replace with your image URL
+            contentDescription = "Profile Image",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(125.dp)
+                .clip(RoundedCornerShape(12.dp))
+        )
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 6.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            Text(
+                text = name,
+                fontSize = 15.sp,
+                fontWeight = FontWeight(600),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2,
+                fontFamily = fontFamily,
+                color = Black50
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back),
+                contentDescription = "back",
+                tint = Dark50
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 6.dp)
+                .clip(shape = RoundedCornerShape(15.dp))
+                .background(White40)
+                .border(
+                    width = 1.dp,
+                    color = Green50,
+                    shape = RoundedCornerShape(15.dp)
+                )
+                .padding(vertical = 4.dp, horizontal = 10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = status,
+                fontSize = 10.sp,
+                fontWeight = FontWeight(500),
+                textAlign = TextAlign.Center,
+                fontFamily = fontFamily,
+                color = Green50
+            )
+        }
+
+        Text(
+            text = location,
+            fontSize = 10.sp,
+            fontWeight = FontWeight(500),
+            textAlign = TextAlign.Center,
+            fontFamily = fontFamily,
+            color = Grey30,
+            modifier = Modifier.padding(horizontal = 6.dp)
+        )
+
+    }
+}

3d808cb3d37ed34edc968ac6489e181b61147352
Baibolat Bekarys <92868206+Be1ba@users.noreply.github.com>
2025-06-19 13:11:40 +0500

    Enhance QR code scanning functionality; update CameraPage to display scanned QR code results in a modal bottom sheet, add validation for QR codes, and implement new color themes for UI elements.

--- app/src/main/java/com/example/bolis/presentation/qr/CameraPage.kt
+++ app/src/main/java/com/example/bolis/presentation/qr/CameraPage.kt
@@ -10,18 +10,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,15 +38,38 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.bolis.R
+import com.example.bolis.ui.Elements.CustomButton
+import com.example.bolis.ui.theme.Glass50
+import com.example.bolis.ui.theme.Green50
+import com.example.bolis.ui.theme.Red40
+import com.example.bolis.ui.theme.Transp50
 import com.example.bolis.ui.theme.White50
 import com.example.bolis.ui.theme.fontFamily
 
+@OptIn(ExperimentalMaterial3Api::class)
+@androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun CameraPage() {
-    val localContext = LocalContext.current
+    val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(localContext)
+        ProcessCameraProvider.getInstance(context)
+    }
+
+    var isSheetOpen by remember { mutableStateOf(false) }
+    var qrCode by remember { mutableStateOf("") }
+    var isQrValid by remember { mutableStateOf(false) }
+
+    val imageAnalysis = remember { ImageAnalysis.Builder().build() }
+    val analyzer = remember {
+        BarcodeAnalyzer(context).apply {
+            onQrScanned = { message ->
+                qrCode = message
+                isQrValid = message.startsWith("5449")
+                isSheetOpen = true
+                imageAnalysis.clearAnalyzer()
+            }
+        }
     }
 
     Box(
@@ -46,19 +78,16 @@ fun CameraPage() {
     ) {
         AndroidView(
             modifier = Modifier.fillMaxSize(),
-            factory = { context ->
+            factory = {
                 val previewView = PreviewView(context)
                 val preview = Preview.Builder().build()
-                val selector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
+                val selector = CameraSelector.DEFAULT_BACK_CAMERA
 
                 preview.setSurfaceProvider(previewView.surfaceProvider)
 
-                val imageAnalysis = ImageAnalysis.Builder().build()
                 imageAnalysis.setAnalyzer(
                     ContextCompat.getMainExecutor(context),
-                    BarcodeAnalyzer(context)
+                    analyzer
                 )
 
                 runCatching {
@@ -71,6 +100,7 @@ fun CameraPage() {
                 }.onFailure {
                     Log.e("CAMERA", "Camera bind error ${it.localizedMessage}", it)
                 }
+
                 previewView
             }
         )
@@ -90,7 +120,6 @@ fun CameraPage() {
                     .height(250.dp)
             )
 
-
             Text(
                 text = "Scan QR Code",
                 fontSize = 24.sp,
@@ -98,6 +127,81 @@ fun CameraPage() {
                 textAlign = TextAlign.Center,
                 fontFamily = fontFamily,
             )
+        }
+
+        if (isSheetOpen) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    isSheetOpen = false
+                    imageAnalysis.setAnalyzer(
+                        ContextCompat.getMainExecutor(context),
+                        analyzer
+                    )
+                }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(48.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(24.dp)
+                ) {
+                    Spacer(Modifier.size(32.dp))
+                    if (isQrValid) {
+                        Text(
+                            text = qrCode,
+                            fontSize = 20.sp,
+                            color = Green50,
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center
+                        )
+                    } else {
+                        Text(
+                            text = "Invalid QR Code",
+                            fontSize = 20.sp,
+                            color = Red40,
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    Spacer(Modifier.size(32.dp))
+
+                    if (isQrValid) {
+                        CustomButton(
+                            onClick = {
+                                // Handle send request
+                            },
+                            name = "Send Request"
+                        )
+//                    } else {
+//                        Text(
+//                            text = "Invalid QR Code",
+//                            fontSize = 18.sp,
+//                            fontWeight = FontWeight(600),
+//                            textAlign = TextAlign.Center,
+//                            fontFamily = fontFamily,
+//                            color = Red40,
+//                        )
+                    }
+
+                    CustomButton(
+                        onClick = {
+                            isSheetOpen = false
+                            imageAnalysis.setAnalyzer(
+                                ContextCompat.getMainExecutor(context),
+                                analyzer
+                            )
+                        },
+                        name = "Scan Again",
+                        backgroundColor = Glass50,
+                        strokeColor = Green50,
+                        textColor = Green50
+                    )
+                    Spacer(Modifier.size(32.dp))
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/bolis/presentation/qr/QRAnalyzer.kt
+++ app/src/main/java/com/example/bolis/presentation/qr/QRAnalyzer.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.example.bolis.data.models.ChatResponse
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
@@ -24,7 +25,7 @@ class BarcodeAnalyzer(private val context: Context) : ImageAnalysis.Analyzer {
     private val options = BarcodeScannerOptions.Builder()
         .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
         .build()
-
+    var onQrScanned: ((String) -> Unit)? = null
     private val scanner = BarcodeScanning.getClient(options)
 
     @SuppressLint("UnsafeOptInUsageError")
@@ -39,70 +40,12 @@ class BarcodeAnalyzer(private val context: Context) : ImageAnalysis.Analyzer {
                     ?.mapNotNull { it.rawValue }
                     ?.joinToString(",")
                     ?.let {
-                        // Handle the scanned QR code here
-                        Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                        onQrScanned?.invoke(it)
+//                        Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
                     }
             }.addOnCompleteListener {
                 imageProxy.close()
             }
-        }
-    }
-}
-
-class QRAnalyzer(
-    private val onQRCodeScanned: (String) -> Unit
-): ImageAnalysis.Analyzer {
-
-    private val supportedImageFormats = listOf(
-        ImageFormat.YUV_420_888,
-        ImageFormat.YUV_422_888,
-        ImageFormat.YUV_444_888
-    )
-
-    override fun analyze(image: ImageProxy) {
-        Log.d("QRAnalyzer", "Image format: ${image.format}")
-        if (image.format in supportedImageFormats) {
-            val yBuffer = image.planes[0].buffer
-            val ySize = yBuffer.remaining()
-            val yBytes = ByteArray(ySize)
-            yBuffer.get(yBytes)
-
-            val source = PlanarYUVLuminanceSource(
-                yBytes,
-                image.width,
-                image.height,
-                0,
-                0,
-                image.width,
-                image.height,
-                true
-            )
-
-            val binaryBitMap = BinaryBitmap(HybridBinarizer(source))
-            try {
-                val reader = MultiFormatReader().apply {
-                    setHints(
-                        mapOf(
-                            DecodeHintType.POSSIBLE_FORMATS to listOf(BarcodeFormat.QR_CODE),
-                            DecodeHintType.TRY_HARDER to true
-                        )
-                    )
-                }
-
-                val result = reader.decodeWithState(binaryBitMap)
-                onQRCodeScanned(result.text)
-            } catch (e: Exception) {
-                Log.d("QRAnalyzer", "No QR found: ${e.message}")
-            } finally {
-                image.close()
-            }
-        }
-    }
-
-    private fun ByteBuffer.toByteArray(): ByteArray {
-        rewind()
-        return ByteArray(remaining()).also {
-            get(it)
         }
     }
 }
